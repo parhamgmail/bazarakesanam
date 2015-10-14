ff712671da18251c95c796cd033d69e64b71462b
parhamgmail <parhamtrojan@hotmail.com>
2015-10-14 20:26:11 +0330

    bazarakesanam
    bazarakesanam

---
+++ js/product.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function(){
+	$('.products').click(function(){
+		var img=$(this).find("img").attr("src");
+		var title=$(this).find("div").text();
+		$('#product .img').attr("src",img);
+		$('#product .title').text(title);
+		$('#gotoproductdetail').html("<a style='display:none;' href='#product' data-transition='slide'></a>");
+		$('#gotoproductdetail a').click();
+	});
+});
---
+++ js/timer.js
@@ -0,0 +1,11 @@
+﻿var myVar = setInterval(function(){ myTimer() }, 6000);
+
+function myTimer() {
+	$('#gotopage2').html("<a style='display:none;' href='#page2' data-transition='slide'></a>");
+	$('#gotopage2 a').click();	
+	myStopFunction();
+}
+
+function myStopFunction() {
+    clearInterval(myVar);
+}
